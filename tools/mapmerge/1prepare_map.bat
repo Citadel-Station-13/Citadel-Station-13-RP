669641eb00557ec7155ce7d7db461b27838b8fd7
GrayRachnid <gray_icy@hotmail.com>
2018-08-14 21:43:30 -0400

    We put our faith in the old mapmerger.

--- tools/mapmerge/1prepare_map.bat
+++ tools/mapmerge/1prepare_map.bat
@@ -1,4 +1,10 @@
-cd ../../maps/tether
+cd ../../maps/northern_star
+
+FOR %%f IN (*.dmm) DO (
+  copy %%f %%f.backup
+)
+
+cd ../../maps/southern_cross
 
 FOR %%f IN (*.dmm) DO (
   copy %%f %%f.backup
