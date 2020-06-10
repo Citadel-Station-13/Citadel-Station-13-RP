eac7b7fb0dbede6f9f6ad4a1378a4717c729c2f9
Letter N <24603524+LetterN@users.noreply.github.com>
2020-06-10 14:56:49 +0800

    (tra)v(i)s things

---
+++ tools/travis/check_changelogs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+# \/ tg's hash
+#"49bc6b1b9ed56c83cceb6674bd97cb34" 
+md5sum -c - <<< "88490b460c26947f5ec1ab1bb9fa9f17 *html/changelogs/example.yml"
+python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
---
+++ tools/travis/check_grep.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -euo pipefail
+
+#nb: must be bash to support shopt globstar
+shopt -s globstar
+
+st=0
+
+if grep -El '^\".+\" = \(.+\)' maps/**/*.dmm;	then
+    echo "Non-TGM formatted map detected. Please convert it using Map Merger!"
+    st=1
+fi;
+if grep -P '^\ttag = \"icon' maps/**/*.dmm;	then
+    echo "tag vars from icon state generation detected in maps, please remove them
+	."
+    st=1
+fi;
+if grep 'step_[xy]' maps/**/*.dmm;	then
+    echo "step_x/step_y variables detected in maps, please remove them."
+    st=1
+fi;
+if grep 'pixel_[xy] = 0' maps/**/*.dmm;	then
+    echo "pixel_x/pixel_y = 0 variables detected in maps, please review to ensure they are not dirty varedits."
+fi;
+# if grep -P '\td[1-2] =' maps/**/*.dmm;	then
+#     echo "d1/d2 cable variables detected in maps, please remove them."
+#     st=1
+# fi;
+if grep '^/area/.+[\{]' maps/**/*.dmm;	then
+    echo "Vareditted /area path use detected in maps, please replace with proper paths."
+    st=1
+fi;
+if grep '\W\/turf\s*[,\){]' maps/**/*.dmm; then
+    echo "base /turf path use detected in maps, please replace with proper paths."
+    st=1
+fi;
+if grep '^/*var/' code/**/*.dm; then
+    echo "Unmanaged global var use detected in code, please use the helpers."
+    st=1
+fi;
+# if grep -i 'centcomm' code/**/*.dm; then
+#     echo "Misspelling(s) of CENTCOM detected in code, please remove the extra M(s)."
+#     st=1
+# fi;
+# if grep -i 'centcomm' _maps/**/*.dmm; then
+#     echo "Misspelling(s) of CENTCOM detected in maps, please remove the extra M(s)."
+#     st=1
+# fi;
+if grep -E '\\\\(red|blue|green|black|b|i[^mc])' code/**/*.dm; then
+	num=`grep -E '\\\\(red|blue|green|black|b|i[^mc])' code/**/*.dm | wc -l`; #bad? yes
+	echo "$num escapes (expecting 4 or less) in the code, please fix this."
+	st=1
+fi;
+
+exit $st
