63d60c21db98bcb733a57d9c131561966e52c57d
Letter N <24603524+LetterN@users.noreply.github.com>
2020-06-10 15:27:31 +0800

    tra:b:is failing, also hacky ways of knowing hsh

--- tools/travis/check_changelogs.sh
+++ tools/travis/check_changelogs.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 # \/ tg's hash
 #"49bc6b1b9ed56c83cceb6674bd97cb34" 
+md5sum "html/changelogs/example.yml"
 md5sum -c - <<< "88490b460c26947f5ec1ab1bb9fa9f17 *html/changelogs/example.yml"
 python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
