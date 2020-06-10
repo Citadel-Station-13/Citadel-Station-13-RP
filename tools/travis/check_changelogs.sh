8538a751d3caea026ffbf5865407ed91337a4dfe
Letter N <24603524+LetterN@users.noreply.github.com>
2020-06-10 15:35:42 +0800

    they're scheming a schema

--- tools/travis/check_changelogs.sh
+++ tools/travis/check_changelogs.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-# \/ tg's hash
-#"49bc6b1b9ed56c83cceb6674bd97cb34" 
-md5sum "html/changelogs/example.yml"
-md5sum -c - <<< "88490b460c26947f5ec1ab1bb9fa9f17 *html/changelogs/example.yml"
+md5sum -c - <<< "4af85ad64b1761a2db1226809459c984 *html/changelogs/example.yml"
 python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
