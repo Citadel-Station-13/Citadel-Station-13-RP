ca11540f7f952428739e66dbf62377c9da50f692
silicons <2003111+silicons@users.noreply.github.com>
2021-03-29 03:37:08 -0700

    md5

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-md5sum -c - <<< "b3c430e884433635c0c0f79f85e14b4e *html/changelogs/example.yml"
+md5sum -c - <<< "364a16f858a957486eaeb1e12673c39c *html/changelogs/example.yml"
 python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
