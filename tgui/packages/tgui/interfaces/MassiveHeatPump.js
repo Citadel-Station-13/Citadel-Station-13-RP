b861695e57d4d6cf8f74856a0ecc8adb2465b88a
LordME <58342752+TheLordME@users.noreply.github.com>
2022-04-26 18:22:04 +0200

    more indentions

--- tgui/packages/tgui/interfaces/MassiveHeatPump.js
+++ tgui/packages/tgui/interfaces/MassiveHeatPump.js
@@ -21,86 +21,86 @@ export const MassiveHeatPump = (props, context) => {
   } = data;
 
   return (
-  <Window
-    width={470}
-    height={270}
-    resizable>
-    <Window.Content>
-      <Section title="Status">
-        <LabeledList>
-          <LabeledList.Item label="Efficiency">
-            <AnimatedNumber value={efficiency * 100} /> % 
-          </LabeledList.Item>
-          <LabeledList.Item label="Current Temperaute">
-            <AnimatedNumber value={current_temp} /> K 
-          </LabeledList.Item>
-          <LabeledList.Item label="Sink Temperature">
-            <AnimatedNumber value={sink_temp} /> K 
-          </LabeledList.Item>
-          <LabeledList.Item label="Load">
-            <ProgressBar
+    <Window
+      width={470}
+      height={270}
+      resizable>
+      <Window.Content>
+        <Section title="Status">
+          <LabeledList>
+            <LabeledList.Item label="Efficiency">
+              <AnimatedNumber value={efficiency * 100} /> % 
+            </LabeledList.Item>
+            <LabeledList.Item label="Current Temperaute">
+              <AnimatedNumber value={current_temp} /> K 
+            </LabeledList.Item>
+            <LabeledList.Item label="Sink Temperature">
+              <AnimatedNumber value={sink_temp} /> K 
+            </LabeledList.Item>
+            <LabeledList.Item label="Load">
+              <ProgressBar
               value={last_power_draw}
               minValue={0}
               maxValue={power_level}
               color={last_power_draw < power_level - 5
               ? 'good' : 'average'}>
-              {formatPower(last_power_draw)}
-            </ProgressBar>
-          </LabeledList.Item>
-        </LabeledList>
-      </Section>
-      <Section
-        title="Controls"
-        buttons={
-          <Button
-          icon="power-off"
-          content={on ? "On" : "Off"}
-          selected={on}
-          onClick={() => act("power")} />
-        }>
-        <LabeledList>
-          <LabeledList.Item label="Target Temperature"
-            buttons={(
-              <Fragment>
-                <Button
-                icon="compress-arrows-alt"
-                content="MIN"
-                onClick={() => act("set_temp", { temp: "min" })} />
-                <Button
-                icon="expand-arrows-alt"
-                content="MAX"
-                onClick={() => act("set_temp", { temp: "max" })} />
-                <Button
-                icon="wrench"
-                content="SET"
-                onClick={() => act("set_temp", { temp: "set" })} />
-              </Fragment>
+                {formatPower(last_power_draw)}
+              </ProgressBar>
+            </LabeledList.Item>
+          </LabeledList>
+        </Section>
+        <Section
+          title="Controls"
+          buttons={
+            <Button
+            icon="power-off"
+            content={on ? "On" : "Off"}
+            selected={on}
+            onClick={() => act("power")} />
+          }>
+          <LabeledList>
+            <LabeledList.Item label="Target Temperature"
+              buttons={(
+                <Fragment>
+                  <Button
+                  icon="compress-arrows-alt"
+                  content="MIN"
+                  onClick={() => act("set_temp", { temp: "min" })} />
+                  <Button
+                  icon="expand-arrows-alt"
+                  content="MAX"
+                  onClick={() => act("set_temp", { temp: "max" })} />
+                  <Button
+                  icon="wrench"
+                  content="SET"
+                  onClick={() => act("set_temp", { temp: "set" })} />
+                </Fragment>
+              )}>
+              {formatSiUnit(target_temp, 0, 'K')}
+            </LabeledList.Item>
+            <LabeledList.Item label="Power Level"
+              buttons={(
+                <Fragment>
+                  <Button
+                  icon="compress-arrows-alt"
+                  content="MIN"
+                  onClick={() => act("set_pow", { pow: "min" })} />
+                  <Button
+                  icon="expand-arrows-alt"
+                  content="MAX"
+                  onClick={() => act("set_pow", { pow: "max" })} />
+                  <Button
+                  icon="wrench"
+                  content="SET"
+                  onClick={() => act("set_pow", { pow: "set" })} />
+                </Fragment>
             )}>
-            {formatSiUnit(target_temp, 0, 'K')}
-          </LabeledList.Item>
-          <LabeledList.Item label="Power Level"
-            buttons={(
-              <Fragment>
-                <Button
-                icon="compress-arrows-alt"
-                content="MIN"
-                onClick={() => act("set_pow", { pow: "min" })} />
-                <Button
-                icon="expand-arrows-alt"
-                content="MAX"
-                onClick={() => act("set_pow", { pow: "max" })} />
-                <Button
-                icon="wrench"
-                content="SET"
-                onClick={() => act("set_pow", { pow: "set" })} />
-              </Fragment>
-            )}>
-            {formatPower(power_level)}
-          </LabeledList.Item>
-        </LabeledList>
-      </Section>
-    </Window.Content>
+              {formatPower(power_level)}
+            </LabeledList.Item>
+          </LabeledList>
+        </Section>
+      </Window.Content>
     </Window>
   );
-  };
+};
   
