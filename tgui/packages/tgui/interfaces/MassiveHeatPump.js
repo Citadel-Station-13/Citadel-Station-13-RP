d86d0426deeb18c75d0f38f195c1a1cd5395e8db
LordME <58342752+TheLordME@users.noreply.github.com>
2022-04-26 18:26:30 +0200

    EVEN MORE INDENTIONS

--- tgui/packages/tgui/interfaces/MassiveHeatPump.js
+++ tgui/packages/tgui/interfaces/MassiveHeatPump.js
@@ -39,11 +39,11 @@ export const MassiveHeatPump = (props, context) => {
             </LabeledList.Item>
             <LabeledList.Item label="Load">
               <ProgressBar
-              value={last_power_draw}
-              minValue={0}
-              maxValue={power_level}
-              color={last_power_draw < power_level - 5
-              ? 'good' : 'average'}>
+                value={last_power_draw}
+                minValue={0}
+                maxValue={power_level}
+                color={last_power_draw < power_level - 5
+                ? 'good' : 'average'}>
                 {formatPower(last_power_draw)}
               </ProgressBar>
             </LabeledList.Item>
@@ -53,27 +53,27 @@ export const MassiveHeatPump = (props, context) => {
           title="Controls"
           buttons={
             <Button
-            icon="power-off"
-            content={on ? "On" : "Off"}
-            selected={on}
-            onClick={() => act("power")} />
+              icon="power-off"
+              content={on ? "On" : "Off"}
+              selected={on}
+              onClick={() => act("power")} />
           }>
           <LabeledList>
             <LabeledList.Item label="Target Temperature"
               buttons={(
                 <Fragment>
                   <Button
-                  icon="compress-arrows-alt"
-                  content="MIN"
-                  onClick={() => act("set_temp", { temp: "min" })} />
+                    icon="compress-arrows-alt"
+                    content="MIN"
+                    onClick={() => act("set_temp", { temp: "min" })} />
                   <Button
-                  icon="expand-arrows-alt"
-                  content="MAX"
-                  onClick={() => act("set_temp", { temp: "max" })} />
+                    icon="expand-arrows-alt"
+                    content="MAX"
+                    onClick={() => act("set_temp", { temp: "max" })} />
                   <Button
-                  icon="wrench"
-                  content="SET"
-                  onClick={() => act("set_temp", { temp: "set" })} />
+                    icon="wrench"
+                    content="SET"
+                    onClick={() => act("set_temp", { temp: "set" })} />
                 </Fragment>
               )}>
               {formatSiUnit(target_temp, 0, 'K')}
@@ -82,19 +82,19 @@ export const MassiveHeatPump = (props, context) => {
               buttons={(
                 <Fragment>
                   <Button
-                  icon="compress-arrows-alt"
-                  content="MIN"
-                  onClick={() => act("set_pow", { pow: "min" })} />
+                    icon="compress-arrows-alt"
+                    content="MIN"
+                    onClick={() => act("set_pow", { pow: "min" })} />
                   <Button
-                  icon="expand-arrows-alt"
-                  content="MAX"
-                  onClick={() => act("set_pow", { pow: "max" })} />
+                    icon="expand-arrows-alt"
+                    content="MAX"
+                    onClick={() => act("set_pow", { pow: "max" })} />
                   <Button
-                  icon="wrench"
-                  content="SET"
-                  onClick={() => act("set_pow", { pow: "set" })} />
+                    icon="wrench"
+                    content="SET"
+                    onClick={() => act("set_pow", { pow: "set" })} />
                 </Fragment>
-            )}>
+              )}>
               {formatPower(power_level)}
             </LabeledList.Item>
           </LabeledList>
