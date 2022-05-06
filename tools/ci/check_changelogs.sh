713f990fba92090066e4e7f28078be36f956b7cd
Zandario <Zandarioh@gmail.com>
2022-05-06 01:35:25 -0500

    oops

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 md5sum -c - <<< "4181a1262fd78f1e10c87cf4d691fa90 *html/changelogs/example.yml"
-python3 tools/ss13_genchangelog.py html/changelog.html html/changelogs
+python3 tools/ss13_genchangelog.py html/changelogs
