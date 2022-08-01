35bd1fae06d80d6e3f9ce8bbcea0eca9c731dd50
VM_USER <VM_USER>
2022-07-31 19:09:26 -0700

    prefixed

---
+++ SQL/database_schema_prefixed.sql
@@ -0,0 +1,192 @@
+/**
+ * make sure to bump schema version and mark changes in database_changelog.md!
+ *
+ * default prefix is citrp_
+ * find replace case sensitive %_PREFIX_%
+ * PRESERVE ANY vr_'s! We need to replace those tables and features at some point, that's how we konw.
+ **/
+
+CREATE TABLE IF NOT EXISTS `citrp_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `rank` varchar(32) NOT NULL DEFAULT 'Administrator',
+  `level` int(2) NOT NULL DEFAULT '0',
+  `flags` int(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `adminckey` varchar(32) NOT NULL,
+  `adminip` varchar(18) NOT NULL,
+  `log` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_ban` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bantime` datetime NOT NULL,
+  `serverip` varchar(32) NOT NULL,
+  `bantype` varchar(32) NOT NULL,
+  `reason` text NOT NULL,
+  `job` varchar(32) DEFAULT NULL,
+  `duration` int(11) NOT NULL,
+  `rounds` int(11) DEFAULT NULL,
+  `expiration_time` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `ip` varchar(32) NOT NULL,
+  `a_ckey` varchar(32) NOT NULL,
+  `a_computerid` varchar(32) NOT NULL,
+  `a_ip` varchar(32) NOT NULL,
+  `who` text NOT NULL,
+  `adminwho` text NOT NULL,
+  `edits` text,
+  `unbanned` tinyint(1) DEFAULT NULL,
+  `unbanned_datetime` datetime DEFAULT NULL,
+  `unbanned_ckey` varchar(32) DEFAULT NULL,
+  `unbanned_computerid` varchar(32) DEFAULT NULL,
+  `unbanned_ip` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `round_id` int(8) NOT NULL,
+  `var_name` varchar(32) NOT NULL,
+  `var_value` int(16) DEFAULT NULL,
+  `details` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+
+CREATE TABLE IF NOT EXISTS `citrp_player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `firstseen` datetime NOT NULL,
+  `lastseen` datetime NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `lastadminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ckey` (`ckey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_poll_option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pollid` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `percentagecalc` tinyint(1) NOT NULL DEFAULT '1',
+  `minval` int(3) DEFAULT NULL,
+  `maxval` int(3) DEFAULT NULL,
+  `descmin` varchar(32) DEFAULT NULL,
+  `descmid` varchar(32) DEFAULT NULL,
+  `descmax` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_poll_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `polltype` varchar(16) NOT NULL DEFAULT 'OPTION',
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `adminonly` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_poll_textreply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `replytext` text NOT NULL,
+  `adminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_poll_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `optionid` int(11) NOT NULL,
+  `ckey` varchar(255) NOT NULL,
+  `ip` varchar(16) NOT NULL,
+  `adminrank` varchar(32) NOT NULL,
+  `rating` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_privacy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `option` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_vr_player_hours` (
+  `ckey` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `department` varchar(64) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `hours` double NOT NULL,
+  PRIMARY KEY (`ckey`,`department`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_death` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `pod` TEXT NOT NULL COMMENT 'Place of death' ,
+  `coord` TEXT NOT NULL COMMENT 'X, Y, Z POD' ,
+  `tod` DATETIME NOT NULL COMMENT 'Time of death' ,
+  `job` TEXT NOT NULL ,
+  `special` TEXT NOT NULL ,
+  `name` TEXT NOT NULL ,
+  `byondkey` TEXT NOT NULL ,
+  `laname` TEXT NOT NULL COMMENT 'Last attacker name' ,
+  `lakey` TEXT NOT NULL COMMENT 'Last attacker key' ,
+  `gender` TEXT NOT NULL ,
+  `bruteloss` INT(11) NOT NULL ,
+  `brainloss` INT(11) NOT NULL ,
+  `fireloss` INT(11) NOT NULL ,
+  `oxyloss` INT(11) NOT NULL ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_karma` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `spendername` TEXT NOT NULL ,
+  `spenderkey` TEXT NOT NULL ,
+  `receivername` TEXT NOT NULL ,
+  `receiverkey` TEXT NOT NULL ,
+  `receiverrole` TEXT NOT NULL ,
+  `receiverspecial` TEXT NOT NULL ,
+  `isnegative` TINYINT(1) NOT NULL ,
+  `spenderip` TEXT NOT NULL ,
+  `time` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_karmatotals` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `byondkey` TEXT NOT NULL ,
+  `karma` INT(11) NOT NULL ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_library` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `author` TEXT NOT NULL ,
+  `title` TEXT NOT NULL ,
+  `content` TEXT NOT NULL ,
+  `category` TEXT NOT NULL ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `citrp_population` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `playercount` INT(11) NULL DEFAULT NULL ,
+  `admincount` INT(11) NULL DEFAULT NULL ,
+  `time` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
