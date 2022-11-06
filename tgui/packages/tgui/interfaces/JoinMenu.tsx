67491a09d99d17f5de9ddb03c02ed8af66d3dbb6
VM_USER <VM_USER>
2022-11-06 04:46:20 -0800

    join menu

--- tgui/packages/tgui/interfaces/JoinMenu.tsx
+++ tgui/packages/tgui/interfaces/JoinMenu.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/jsx-max-depth */
 import { Fragment } from 'inferno';
 import { useBackend } from '../backend';
-import { Box, Button, Section, Flex, Icon, NoticeBox, Collapsible, Stack } from '../components';
-import { COLORS } from '../constants';
+import { Box, Button, Section, Icon, NoticeBox, Collapsible, Stack } from '../components';
 import { Window } from '../layouts';
 
 interface JoinableRoles {
@@ -43,6 +42,14 @@ interface JoinMenuData {
   charname: string;
   queue?: number;
 }
+
+interface JoinFactionProps {
+  faction: string;
+  departments: {
+    [key: string]: JoinableJob[]
+  }
+}
+
 // LateChoices
 export const JoinMenu = (props, context) => {
   const { act, data } = useBackend<JoinMenuData>(context);
@@ -52,171 +59,120 @@ export const JoinMenu = (props, context) => {
       <Window.Content>
         <Stack vertical fill>
           <Stack.Item>
-            <Section>
-              Welcome!
+            <Section fill>
+              <h1>Welcome, {data.charname}!</h1>
+              Round Duration: {data.duration}
+              Security Level: {data.security_level}
+              {!!data.evacuated && (
+                <NoticeBox
+                  info={data.evacuated === 2}
+                  warning={data.evacuated === 1 || data.evacuated === 3}>
+                  {(data.evacuated === 2)? "A crew transfer is in progress."
+                    : ((data.evacuated === 3)? "The installation has been evacuated."
+                      : "An evacuation is in progress.")}
+                </NoticeBox>
+              )}
             </Section>
           </Stack.Item>
           <Stack.Item>
-            Station Roles
+            {
+              Object.entries(data.jobs).map(([k, v]) => {
+                <JoinFaction faction={k} departments={v} />;
+              })
+            }
           </Stack.Item>
           <Stack.Item>
-            Ghost Roles
+            <Section title="Ghost Roles">
+              {
+                data.ghostroles.map((role) => {
+                  return (
+                    <Collapsible key={role.id} title={role.name} color="transparent" buttons={
+                      <>{(role.slots === -1)? '' : role.slots} <Icon name="user-friends" />
+                        <Button.Confirm
+                          icon="sign-in-alt"
+                          content="Join"
+                          color="transparent"
+                          onClick={() => act('join', { id: role.id, type: "ghostrole" })} />
+                      </>
+                    }>
+                      <Box>
+                        {role.desc}
+                      </Box>
+                    </Collapsible>
+                  );
+                })
+              }
+            </Section>
           </Stack.Item>
         </Stack>
-        <Flex fill direction="column">
-          {/* Top bar */}
-          <Flex.Item mb={1}>
-            <Flex direction="column">
-              <Flex.Item grow>
-                <Section fill>
-                  Round Duration: {data.duration}
-                </Section>
-              </Flex.Item>
-              <Flex.Item grow>
-                <Section fill>
-                  <NoticeBox
-                    success={data.security_level === "green"}
-                    info={data.security_level === "blue"}
-                    warning={data.security_level === "yellow" || data.security_level === "violet"
-                    || data.security_level === "orange"}
-                    danger={data.security_level === "red" || data.security_level === "delta"}>
-                    Security Level: {data.security_level.charAt(0).toUpperCase()
-                      + data.security_level.slice(1)}
-                  </NoticeBox>
-                  {!!data.evacuated && (
-                    <NoticeBox
-                      info={data.evacuated === 2}
-                      warning={data.evacuated === 1 || data.evacuated === 3}>
-                      The shuttle has {data.evacuated === 3 ? "left the station." : "been called."}
-                    </NoticeBox>
-                  )}
-                </Section>
-              </Flex.Item>
-            </Flex>
-          </Flex.Item>
-          {/* Join things */}
-          <Flex.Item mb={1}>
-            {
-              Object.entries(data.jobs).map(([k, v]) => (
-                <RoleList faction={k} joinableJobs={v} key={k} />
-              ))
-            }
-          </Flex.Item>
-          {/* Latejoin */}
-          <Flex.Item mb={1}>
-            {/* Ghostroles */}
-            <Section title="Ghostroles">
-              {data.ghostroles.map(
-                (role) => (
-                  <Collapsible key={role.id} title={role.name} color="transparent" buttons={
-                    <>
-                      {(role.slots === -1)? '' : role.slots} <Icon name="user-friends" />
-                      <Button.Confirm
-                        icon="sign-in-alt"
-                        content="Join"
-                        color="transparent"
-                        onClick={() => act('join', { id: role.id, type: "ghostrole" })}
-                      />
-                    </>
-                  }>
-                    <Box mt={1}>
-                      {role.desc}
-                    </Box>
-                  </Collapsible>
-                ))}
-            </Section>
-          </Flex.Item>
-        </Flex>
       </Window.Content>
     </Window>
   );
 };
 
-/**
- * stupid way of recasting the department into colors
- */
-const recast_this_stupid_shit = {
-  supply: "cargo",
-  command: "captain",
-  service: "other",
-  civillian: "other",
-  miscellaneous: "other",
-  medical: "medbay",
-  engineering: "engineering",
-  science: "science",
-  security: "security",
-};
-
-/**
- * EXPECTED ORDER:
- * command silicon medical
- * supply service science
- * engineering msic security
- */
-const sortWeight = {
-  command: 12,
-  civillian: 11,
-  medical: 10,
-  supply: 9,
-  service: 8,
-  science: 7,
-  engineering: 6,
-  miscellaneous: 5,
-  security: 4,
-  silicons: 3,
-};
-
-interface IRoleListProps {
-  faction: string;
-  joinableJobs: {
-    [key: string]: JoinableJob[]
-  }
-}
-
-const RoleList = (props: IRoleListProps, context) => {
+const JoinFaction = (props: JoinFactionProps, context) => {
   const { act } = useBackend(context);
-
-  const orderedData = Object.keys(props.joinableJobs).sort((a, b) => {
-    const depA = a.toLowerCase();
-    const depB = b.toLowerCase();
-    if (!(depB in sortWeight) || !(depA in sortWeight)) {
+  const ordered = Object.keys(props.departments).sort((a, b) => {
+    const A = a.toLowerCase();
+    const B = b.toLowerCase();
+    let hasA = (A in sortWeight);
+    let hasB = (B in sortWeight);
+    if (hasA && !hasB) {
+      return 1;
+    }
+    else if (!hasA && hasB) {
+      return -1;
+    }
+    else if (!hasA && !hasB) {
       return 0;
     }
-    return sortWeight[depB] - sortWeight[depA];
+    else {
+      return sortWeight[B] - sortWeight[A];
+    }
   });
 
   return (
-    <Section title={`${props.faction} Roles`}>
-      <Flex
-        justify="space-evenly"
-        align="baseline"
-        wrap
-        grow
-      >
-        {orderedData.map(name => {
-          const data: JoinableJob[] = props.joinableJobs[name];
-
+    <Collapsible title={`${props.faction} Roles`}>
+      {
+        ordered.map((depName) => {
+          const jobs: JoinableJob[] = props.departments[depName];
           return (
-            <Flex.Item ml={2} mr={2} mt={2} key={name} basis="22%" grow>
-              <Section
-                title={name}
-                borderHexColor={
-                  COLORS.department[recast_this_stupid_shit[name.toLowerCase()]]
-                }>
-                {data.map(dat => (
-                  <Button.Confirm
-                    fluid
-                    key={dat.id}
-                    tooltip={dat.desc}
-                    content={`${dat.real_name} - ${dat.slots === -1 ? "Unlimited" : dat.slots}`}
-                    confirmContent={`Play as ${dat.real_name}?`}
-                    onClick={() => act('join', { 'id': dat.id, 'type': 'job' })}
-                  />
-                ))}
-              </Section>
-            </Flex.Item>
+            <Collapsible color="transparent" key={depName} title={depName}>
+              {
+                jobs.map((job) => {
+                  return (
+                    <Collapsible color="transparent" key={job.id} title={job.name} buttons={
+                      <>{(job.slots === -1)? '' : job.slots} <Icon name="user-friends" />
+                        <Button.Confirm
+                          icon="sign-in-alt"
+                          content="Join"
+                          color="transparent"
+                          onClick={() => act('join', { id: job.id, type: "job" })} />
+                      </>
+                    }>
+                      {job.desc}
+                    </Collapsible>
+                  );
+                })
+              }
+            </Collapsible>
           );
-        })}
-      </Flex>
-    </Section>);
+        })
+      }
+    </Collapsible>
+  );
+};
+
+const sortWeight = {
+  command: 100,
+  security: 90,
+  engineering: 70,
+  medical: 80,
+  science: 70,
+  exploration: 60,
+  civillian: 50,
+  misc: 40,
+  miscellaneous: 40,
+  "off-duty": 30,
+  silicons: 20,
 };
