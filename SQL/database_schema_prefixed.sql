44caa14c648a88cc1cb084d305fc2887add17ad8
silicons <2003111+silicons@users.noreply.github.com>
2022-11-25 14:55:53 -0800

    Update database_schema_prefixed.sql

--- SQL/database_schema_prefixed.sql
+++ SQL/database_schema_prefixed.sql
@@ -238,11 +238,11 @@ CREATE TABLE IF NOT EXISTS `rp_connection_log` (
 ----- Object Persistence Store -----
 -- These are not multi-server synchronization safe! It is expected that you DO NOT share these databases --
 CREATE TABLE IF NOT EXISTS `rp_persist_keyed_strings` (
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` datetime NOT NULL,
-  `key` varchar(64) NOT NULL,
+  `created` DATETIME NOT NULL DEFAULT Now(),
+  `modified` DATETIME NOT NULL,
+  `key` VARCHAR(64) NOT NULL,
   `value` MEDIUMTEXT NULL,
-  `group` varchar(64) NULL,
+  `group` VARCHAR(64) NULL,
   `revision` INT(11) NOT NULL,
   PRIMARY KEY(`key`, `group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
