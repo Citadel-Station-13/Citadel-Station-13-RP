b21ec6ca724c6f9398a95c5e8b7928df018c96ac
silicons <no@you.cat>
2023-02-06 18:04:45 -0800

    staging

--- tgui/packages/tgui/interfaces/AirlockElectronics.tsx
+++ tgui/packages/tgui/interfaces/AirlockElectronics.tsx
@@ -1,15 +1,14 @@
+/*
 import { useBackend } from '../backend';
-import { Button, Input, LabeledList, Section } from '../components';
+import { Button, LabeledList, Section } from '../components';
 import { Window } from '../layouts';
-import { AccessConfig } from './common/AccessConfig';
+import { AccessList } from './common/AccessList';
 
 export const AirlockElectronics = (props, context) => {
   const { act, data } = useBackend(context);
   const {
     oneAccess,
     unres_direction,
-    passedName,
-    passedCycleId,
   } = data;
   const regions = data.regions || [];
   const accesses = data.accesses || [];
@@ -58,27 +57,9 @@ export const AirlockElectronics = (props, context) => {
                   unres_direction: '8',
                 })} />
             </LabeledList.Item>
-            <LabeledList.Item
-              label="Airlock Name">
-              <Input fluid
-                maxLength={30}
-                value={passedName}
-                onChange={(e, value) => act('passedName', {
-                  passedName: value,
-                })} />
-            </LabeledList.Item>
-            <LabeledList.Item
-              label="Cycling Id">
-              <Input fluid
-                maxLength={30}
-                value={passedCycleId}
-                onChange={(e, value) => act('passedCycleId', {
-                  passedCycleId: value,
-                })} />
-            </LabeledList.Item>
           </LabeledList>
         </Section>
-        <AccessConfig
+        <AccessList
           accesses={regions}
           selectedList={accesses}
           accessMod={ref => act('set', {
@@ -96,3 +77,4 @@ export const AirlockElectronics = (props, context) => {
     </Window>
   );
 };
+*/
