993f354c158c7765733e44df706522db15738340
silicons <2003111+silicons@users.noreply.github.com>
2023-06-16 04:11:20 +0000

    scream

---
+++ tgui/packages/tgui/interfaces/Journalism/data.tsx
@@ -0,0 +1,23 @@
+export interface JournalismProps {
+
+}
+
+export interface JournalismData {
+
+}
+
+export interface NewsComment {
+
+}
+
+export interface NewsPost {
+
+}
+
+export interface NewsChannel {
+
+}
+
+export interface NewsNetwork {
+  
+}
---
+++ tgui/packages/tgui/interfaces/Journalism/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * master interface for newscaster networks / systems
+ */
+
+import { Window } from "../../layouts";
+import { JournalismProps } from "./data";
+
+
+export const Journalism = (props: JournalismProps, context) => {
+  return (
+    <Window>
+      test
+    </Window>2
+  );
+};
