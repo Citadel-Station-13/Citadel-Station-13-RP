99f252eea1738ca6d125e45fe4e5933927b902ab
silicons <no@you.cat>
2023-07-07 13:28:31 -0700

    move files

--- tgui/packages/tgui/interfaces/common/Color.tsx
+++ tgui/packages/tgui/interfaces/common/Color.tsx
@@ -1,3 +1,7 @@
+import { Component } from "inferno";
+import { Box, Section, Tabs } from "../../components";
+import { BoxProps } from "../../components/Box";
+
 // full, 20-value RGBA matrix with constants
 export type ByondColorMatrixRGBAC = [
   number, number, number, number,
@@ -29,3 +33,55 @@ export type ByondColorMatrixRGB = [
   number, number, number,
   number, number, number,
 ];
+
+// RGB or RGBA string, usually used to do color multiply operations / set color vars.
+export type ByondColorString = string;
+
+// byond color var compatible
+export type ByondAtomColor =
+  ByondColorString |
+  ByondColorMatrixRGB |
+  ByondColorMatrixRGBA |
+  ByondColorMatrixRGBAC |
+  ByondColorMatrixRGBC;
+
+interface ColorPickerProps extends BoxProps {
+  allowMatrix: boolean;
+  currentColor: ByondAtomColor;
+  setColor: (ByondAtomColor) => void;
+}
+
+interface ColorPickerState {
+  mode: ColorPickerMode;
+}
+
+enum ColorPickerMode {
+  Normal = 0,
+  Matrix = 1,
+}
+
+export class ColorPicker extends Component<ColorPickerProps, ColorPickerState> {
+  state = {
+    mode: ColorPickerMode.Normal,
+  };
+
+  render() {
+    return (
+      <Box {...this.props}>
+        <Tabs>
+          <Tabs.Tab
+            selected={this.state.mode === ColorPickerMode.Normal}
+            onClick={() => this.setState((prev) => ({ ...prev, mode: ColorPickerMode.Normal }))}>Basic
+          </Tabs.Tab>
+          <Tabs.Tab
+            selected={this.state.mode === ColorPickerMode.Matrix}
+            onClick={() => this.setState((prev) => ({ ...prev, mode: ColorPickerMode.Matrix }))}>Matrix
+          </Tabs.Tab>
+        </Tabs>
+        <Section>
+          Unimplemented
+        </Section>
+      </Box>
+    );
+  }
+}
