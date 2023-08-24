6334924e95d581d1267a4acf02efeb368bcf8feb
Timothy Teakettle <59849408+timothyteakettle@users.noreply.github.com>
2023-08-24 23:23:00 +0100

    update table definition

--- SQL/database_schema.sql
+++ SQL/database_schema.sql
@@ -360,7 +360,7 @@ CREATE TABLE IF NOT EXISTS `%_PREFIX_%population` (
   -- Persistence - Character Serialization --
   CREATE TABLE IF NOT EXISTS `%_PREFIX_%character_lookup` (
   `character_name` VARCHAR(128) NOT NULL ,
-  `character_id` `id` int(11) NOT NULL AUTO_INCREMENT,
+  `character_id` int(11) NOT NULL AUTO_INCREMENT ,
   `player_id` VARCHAR(128) NOT NULL ,
   `character_type` VARCHAR(64) NOT NULL,
   PRIMARY KEY (`player_id`, `character_name`)
