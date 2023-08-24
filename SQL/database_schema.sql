dc4171cca607dce8c29a3f7acfcb33639e7f423f
Timothy Teakettle <59849408+timothyteakettle@users.noreply.github.com>
2023-08-24 23:34:19 +0100

    char id isnt a primary key so it cant be an auto increment

--- SQL/database_schema.sql
+++ SQL/database_schema.sql
@@ -360,9 +360,9 @@ CREATE TABLE IF NOT EXISTS `%_PREFIX_%population` (
   -- Persistence - Character Serialization --
 CREATE TABLE IF NOT EXISTS `%_PREFIX_%character_lookup` (
   `character_name` VARCHAR(128) NOT NULL ,
-  `character_id` int(11) NOT NULL AUTO_INCREMENT ,
+  `character_id` VARCHAR(257) NOT NULL ,
   `player_id` VARCHAR(128) NOT NULL ,
   `character_type` VARCHAR(64) NOT NULL ,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`character_id`),
   UNIQUE (`player_id`, `character_name`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
