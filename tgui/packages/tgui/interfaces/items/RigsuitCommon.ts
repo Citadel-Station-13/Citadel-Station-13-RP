fb13def20d4eb29735cea1791650adb8e03493b6
silicons <no@you.cat>
2024-01-03 15:49:06 -0800

    Fix

--- tgui/packages/tgui/interfaces/items/RigsuitCommon.ts
+++ tgui/packages/tgui/interfaces/items/RigsuitCommon.ts
@@ -1,3 +1,5 @@
+import { BooleanLike } from "common/react";
+
 export enum RigControlFlags {
   None = 0,
   Movement = (1<<0),
@@ -80,3 +82,51 @@ export const RigUIZoneSelection:
       key: "misc",
     },
   ];
+
+export type RigModuleDynamicSchema = (
+  RigModuleDynamicSchemaAction |
+  RigModuleDynamicSchemaActionSection
+)[];
+
+export type RigModuleDynamicSchemaBase = {
+  id: string;
+}
+export type RigModuleDynamicSchemaAction = RigModuleDynamicSchemaBase & {
+  type: "action";
+  name: string;
+  icon: string | null | undefined;
+  confirm: BooleanLike;
+  bindable: BooleanLike;
+  confirmText: string | null | undefined;
+  confirmIcon: string | null | undefined;
+}
+
+export type RigModuleDynamicSchemaActionSection = {
+  type: "section";
+  actions: RigModuleDynamicSchemaAction[]
+};
+
+export type RigModuleDynamicConfig = (
+  RigModuleDynamicConfigNumber |
+  RigModuleDynamicConfigString |
+  RigModuleDynamicConfigToggle
+)[];
+
+export type RigModuleDynamicConfigBase = {
+  name: string;
+  key: string;
+}
+export type RigModuleDynamicConfigToggle = RigModuleDynamicConfigBase & {
+  type: "toggle";
+};
+export type RigModuleDynamicConfigNumber = RigModuleDynamicConfigBase & {
+  type: "number";
+  min: number;
+  max: number;
+  round: number | null | undefined;
+};
+export type RigModuleDynamicConfigString = RigModuleDynamicConfigBase & {
+  type: "string";
+  maxLength: number;
+  alphanumeric: BooleanLike;
+}
