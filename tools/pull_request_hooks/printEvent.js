4d6732bb967dfb70ebe74f6b1e9a50b4644a7e6d
LordME <58342752+TheLordME@users.noreply.github.com>
2024-06-02 16:18:45 +0200

    remove the action debugging messages again

--- tools/pull_request_hooks/printEvent.js
+++
@@ -1,6 +0,0 @@
-// printEvent.js
-//const { toJson } = require('your-toJson-module'); // Replace 'your-toJson-module' with the actual module name
-
-const eventJson = process.argv[2];
-const eventObject = JSON.parse(eventJson);
-console.log(toJson(eventObject));
