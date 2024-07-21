5ac2f45f61ce8d689ca0bba862dd0711d3675ac2
Nyeogmi <economicsbat@gmail.com>
2024-07-20 21:27:02 -0700

    Use Node 20

--- dependencies.sh
+++ dependencies.sh
@@ -14,8 +14,8 @@ export BYOND_MINOR=1636
 export RUST_G_VERSION=3.0.0-experimental-graph-ops
 
 #node version
-export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+export NODE_VERSION=20
+export NODE_VERSION_PRECISE=20.15.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.8
