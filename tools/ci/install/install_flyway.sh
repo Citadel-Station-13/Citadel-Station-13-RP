1b8671fed0eb744cf5a0ba0283008d9060f2ab0b
silicons <2003111+silicons@users.noreply.github.com>
2024-12-09 22:28:19 -0500

    update path

--- tools/ci/install/install_flyway.sh
+++ tools/ci/install/install_flyway.sh
@@ -10,8 +10,6 @@ else
 	rm -rf ~/flyway/flyway-$FLYWAY_VERSION
 	mkdir ~/flyway
 	cd ~/flyway
-	curl "https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/$FLYWAY_VERSION/flyway-commandline-$FLYWAY_VERSION-linux-x64.tar.gz" -o flyway.zip
-	unzip flyway.zip
-	rm flyway.zip
+	curl "https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/$FLYWAY_VERSION/flyway-commandline-$FLYWAY_VERSION-linux-x64.tar.gz" | tar -xvz
 	cd ~
 fi
