9e32a8bbcb0b94198d597793bd92a558d7fa5e0e
Letter N <24603524+LetterN@users.noreply.github.com>
2025-03-25 13:29:26 +0800

    The tgui update thing (#7042)
    includes
    https://github.com/Citadel-Station-13/Citadel-Station-13-RP/pull/7040
    https://github.com/Citadel-Station-13/Citadel-Station-13-RP/pull/7036
    
    update chat highlight, update saving so you can save and export and make
    storage support 516 properly this time surely
    
    also typescriptify `tgui-chat`

--- tgui/packages/tgui-panel/ping/reducer.ts
+++ tgui/packages/tgui-panel/ping/reducer.ts
@@ -8,15 +8,22 @@ import { clamp01, scale } from 'common/math';
 import { pingFail, pingSuccess } from './actions';
 import { PING_MAX_FAILS, PING_ROUNDTRIP_BEST, PING_ROUNDTRIP_WORST } from './constants';
 
-export const pingReducer = (state = {}, action) => {
+type PingState = {
+  roundtrip: number | undefined;
+  roundtripAvg: number | undefined;
+  failCount: number;
+  networkQuality: number;
+};
+
+export const pingReducer = (state = {} as PingState, action) => {
   const { type, payload } = action;
 
   if (type === pingSuccess.type) {
     const { roundtrip } = payload;
     const prevRoundtrip = state.roundtripAvg || roundtrip;
     const roundtripAvg = Math.round(prevRoundtrip * 0.4 + roundtrip * 0.6);
-    const networkQuality = 1 - scale(roundtripAvg,
-      PING_ROUNDTRIP_BEST, PING_ROUNDTRIP_WORST);
+    const networkQuality =
+      1 - scale(roundtripAvg, PING_ROUNDTRIP_BEST, PING_ROUNDTRIP_WORST);
     return {
       roundtrip,
       roundtripAvg,
@@ -27,9 +34,10 @@ export const pingReducer = (state = {}, action) => {
 
   if (type === pingFail.type) {
     const { failCount = 0 } = state;
-    const networkQuality = clamp01(state.networkQuality
-      - failCount / PING_MAX_FAILS);
-    const nextState = {
+    const networkQuality = clamp01(
+      state.networkQuality - failCount / PING_MAX_FAILS,
+    );
+    const nextState: PingState = {
       ...state,
       failCount: failCount + 1,
       networkQuality,
