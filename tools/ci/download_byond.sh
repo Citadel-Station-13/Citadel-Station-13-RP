914bb3aed4315db6a318774d4ab5eb69979718c6
Zandario <zandarioh@gmail.com>
2025-06-03 19:32:55 -0400

    Begging that this downloads

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -3,4 +3,4 @@ set -e
 source dependencies.sh
 
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl -H "User-Agent: citadelstation13/1.0 CI Script" "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
+curl -H "User-Agent: citadelstation/1.0 CI Script" "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
