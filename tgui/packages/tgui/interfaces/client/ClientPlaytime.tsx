2d6dc7ec9b6985d9866a8ba461695ae2b107f6c1
silicons <2003111+silicons@users.noreply.github.com>
2025-06-18 19:14:33 -0400

    Fix

--- tgui/packages/tgui/interfaces/client/ClientPlaytime.tsx
+++ tgui/packages/tgui/interfaces/client/ClientPlaytime.tsx
@@ -5,7 +5,7 @@
 
 import { round } from "common/math";
 import { useBackend } from "../../backend";
-import { NoticeBox, ProgressBar, Section, Stack } from "../../components";
+import { LabeledList, NoticeBox, ProgressBar, Section } from "../../components";
 import { Window } from "../../layouts";
 
 interface ClientPlaytimeData {
@@ -24,20 +24,21 @@ export const ClientPlaytime = (props, context) => {
   }
 
   const itIsntNullDamnit: Record<string, number> = data.playtime as any;
-  let highest: number = Object.values(data.playtime).reduce((a, b) => Math.max(a, b));
+  const rawPlaytimeNumbers = Object.values(data.playtime);
+  let highest: number = rawPlaytimeNumbers.length ? rawPlaytimeNumbers.reduce((a, b) => Math.max(a, b)) : 1;
   let sorted = Object.keys(data.playtime).toSorted((a, b) => itIsntNullDamnit[a] - itIsntNullDamnit[b]);
 
   return (
     <Window width={400} height={800} title="Playtime Viewer">
       <Window.Content>
         <Section title="Tracked Playtime">
-          <Stack vertical fill>
+          <LabeledList>
             {sorted.map((key) => (
-              <Stack.Item key={key}>
+              <LabeledList.Item key={key} label={key}>
                 <ProgressBar value={itIsntNullDamnit[key]} maxValue={highest}>{`${round(itIsntNullDamnit[key] / 60, 0.1)}h`}</ProgressBar>
-              </Stack.Item>
+              </LabeledList.Item>
             ))}
-          </Stack>
+          </LabeledList>
         </Section>
       </Window.Content>
     </Window>
