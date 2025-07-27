20e678a05ee001594bb551c330d907e8c9a83d84
silicons <2003111+silicons@users.noreply.github.com>
2025-07-27 07:50:27 -0700

    t

--- tgui/packages/tgui/components/VScrollingWindower.tsx
+++ tgui/packages/tgui/components/VScrollingWindower.tsx
@@ -3,15 +3,20 @@
  * @license MIT
  */
 
-import { Component } from "inferno";
+import { Component, InfernoNode } from "inferno";
 import { Box, BoxProps } from "./Box";
 
 /**
  * Low-level vertical virtual scrolling window component.
  * Allows putting a ton of stuff in a single scrollable div by not actually rendering all of it.
+ *
+ * @prop rowCount number of rows total
+ * @prop rowHeight height of a row, as either a string for css or a number.
  */
 export class VScrollingWindower extends Component<{
-
+  rowCount: number;
+  rowHeight: string | number;
+  rowCall: (r: number) => InfernoNode;
 } & BoxProps> {
   state: {
 
@@ -25,12 +30,12 @@ export class VScrollingWindower extends Component<{
   }
 
   render() {
-
+    let innerContents;
 
     return (
-      <Box className="VirtualScrolContainer">
-        <Box className="VirtualScrollInner">
-          {}
+      <Box className="VScrollingWindower">
+        <Box className="VScrollingWindower__Inner">
+          {innerContents}
         </Box>
       </Box>
     );
