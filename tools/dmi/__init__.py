2e23bfb816982e07b3365ae8bec2335da490054d
silicons <2003111+silicons@users.noreply.github.com>
2025-12-21 23:39:10 -0800

    Fix

--- tools/dmi/__init__.py
+++ tools/dmi/__init__.py
@@ -381,27 +381,33 @@ class DmiTransformPipeline:
 
         return image
 
-    def shift_crop_by_pattern(self, pattern: str) -> DmiTransformPipeline:
+    def shift_crop_by_pattern(self, pattern: str, x_o: int, y_o: int) -> DmiTransformPipeline:
         matcher: re.Pattern = re.compile(pattern)
-        self.dmi.states = [DmiTransformPipeline.impl_shift_crop(s, x_o, y_o) if matcher.fullmatch(s.name) else s for s in self.dmi.states]
+        self.dmi.states = [DmiTransformPipeline.impl_shift_crop_state(s, x_o, y_o) if matcher.fullmatch(s.name) else s for s in self.dmi.states]
         return self
 
-    def impl_shift_crop_state(self, state: State, x_o: int, y_o: int):
+    def impl_shift_crop_state(state: State, x_o: int, y_o: int):
         state.frames = [DmiTransformPipeline.impl_shift_crop_image(f, x_o, y_o) for f in state.frames]
         return state
 
-    def impl_shift_crop_image(self, image: Image.Image, x_o: int, y_o: int):
+    def impl_shift_crop_image(image: Image.Image, x_o: int, y_o: int):
         image2 = image.copy()
+        # clear
+        for x in range(image.width):
+            for y in range(image.height):
+                image2.putpixel((x, y), (0, 0, 0, 0))
+        # copy
         for x in range(image.width):
             for y in range(image.height):
                 o_x = x + x_o
-                o_y = y + y_o
-                if(o_x > image.width or o_x < 1):
+                # negative because the image row is top first
+                o_y = y - y_o
+                if(o_x >= image.width or o_x < 0):
                     continue
-                if(o_y > image.width or o_y < 1):
+                if(o_y >= image.width or o_y < 0):
                     continue
                 image2.putpixel((o_x, o_y), image.getpixel((x, y)))
-        return image
+        return image2
 
     def greyscale_state_pattern(self, pattern: str) -> DmiTransformPipeline:
         matcher: re.Pattern = re.compile(pattern)
