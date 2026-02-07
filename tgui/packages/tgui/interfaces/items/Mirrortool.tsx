e9edf8adea55ac1b33426e3b5ab2da39fc52dc6a
silicons <2003111+silicons@users.noreply.github.com>
2026-02-07 09:33:10 -0800

    mirrortool ui

--- tgui/packages/tgui/interfaces/items/Mirrortool.tsx
+++ tgui/packages/tgui/interfaces/items/Mirrortool.tsx
@@ -3,25 +3,37 @@
  * @license MIT
  */
 
-import { BooleanLike } from "tgui-core/react"
-import { ResleevingMirrorData } from "../common/Resleeving"
-import { Window } from "../../layouts";
-import { Section } from "tgui-core/components";
-import { useBackend } from "../../backend";
+import { ResleevingMirror, ResleevingMirrorData } from '../common/Resleeving';
+import { Window } from '../../layouts';
+import { Button, Dimmer, NoticeBox, Section } from 'tgui-core/components';
+import { useBackend } from '../../backend';
 
 interface MirrortoolContext {
   mirror: ResleevingMirrorData;
-};
+}
 
 export const Mirrortool = (props) => {
-  const {act, data} = useBackend<MirrortoolContext>();
+  const { act, data } = useBackend<MirrortoolContext>();
   return (
     <Window title="Mirror Tool">
       <Window.Content>
-        <Section title="Mirror">
-          test
+        <Section
+          title="Mirror"
+          buttons={
+            <Button onClick={() => act(data.mirror ? 'eject' : 'insert')}>
+              {data.mirror ? 'Remove' : 'Insert'}
+            </Button>
+          }
+        >
+          {data.mirror ? (
+            <ResleevingMirror data={data.mirror}></ResleevingMirror>
+          ) : (
+            <Dimmer>
+              <NoticeBox>Mirror slot empty</NoticeBox>
+            </Dimmer>
+          )}
         </Section>
       </Window.Content>
     </Window>
-  )
-}
+  );
+};
